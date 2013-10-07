f3ad914fef8262a834ef6e67e5ed4ada915386df
Craig Walls <cwalls@vmware.com>
2013-10-06 22:40:13 -0500

    Extracted contact class and JDBC code into separate files.

--- contacts/app.groovy
+++ contacts/app.groovy
@@ -2,42 +2,23 @@
 @Grab("org.thymeleaf:thymeleaf-spring3:2.0.16")
 @Grab("com.h2database:h2:1.3.173")
 
-import java.sql.ResultSet
-
 @Controller
 @RequestMapping("/")
 class App {
 
   @Autowired
-  JdbcTemplate jdbc
+  ContactRepository contactRepo
 
   @RequestMapping(method=RequestMethod.GET)
   String home(Map<String,Object> model) {
-
-    List<Contact> contacts = jdbc.query(
-      "select id, firstName, lastName, phoneNumber, emailAddress from contacts order by lastName", 
-      new RowMapper<Contact>() {
-        Contact mapRow(ResultSet rs, int rowNum) {
-          new Contact(id: rs.getLong(1), firstName: rs.getString(2), lastName: rs.getString(3), phoneNumber: rs.getString(4), emailAddress: rs.getString(5))
-        }
-      })
-
+    List<Contact> contacts = contactRepo.findAll()
     model.putAll([contacts: contacts])
     return "home"
   }
 
   @RequestMapping(method=RequestMethod.POST)
   String submit(Contact contact) {
-    jdbc.update("insert into contacts (firstName, lastName, phoneNumber, emailAddress) values (?, ?, ?, ?)",
-      contact.firstName, contact.lastName, contact.phoneNumber, contact.emailAddress)
+    contactRepo.save(contact)
     return "redirect:/"
   }
-}
-
-class Contact {
-  long id
-  String firstName
-  String lastName
-  String phoneNumber
-  String emailAddress
 }
