026427d44346f6c9066fb8f6043b5ae6a07435dd
Craig Walls <cwalls@gopivotal.com>
2014-06-11 15:12:55 -0500

    cleanup

--- web-jsp/home.groovy
+++
@@ -1,8 +0,0 @@
-@Grab("org.springframework.boot:spring-boot-starter-actuator:0.5.0.M5")
-@Controller
-class HomeController {
-	@RequestMapping("/")
-	String home() {
-		return "home";
-	}
-}
--- web-thymeleaf/home.groovy
+++
@@ -1,9 +0,0 @@
-@Grab("org.springframework.boot:spring-boot-starter-actuator:0.5.0.M5")
-@Grab("org.thymeleaf:thymeleaf-spring3:2.0.16")
-@Controller
-class HomeController {
-	@RequestMapping("/")
-	String home() {
-		return "home";
-	}
-}
